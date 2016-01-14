c15f129bd26034b9eca270a172999c6670bcad16
Sven Bock <sb@generationrobots.com>
2016-01-14 12:10:52 +0100

    added alias to baxter.sh supports also the parameter for sim

--- rosbash.bash
+++ rosbash.bash
@@ -84,3 +84,9 @@ _urdfshow() {
 alias urdfshow=_urdfshow
 alias make-eclipse-project='cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug'
 alias rosdep_indigo='rosdep install -r --from-paths src --ignore-src --rosdistro indigo -y'
+
+myBaxter() {
+  cd ~/ros_ws   #change this path according to your baxter workspace
+  ./baxter.sh $1
+}
+alias baxter=myBaxter
